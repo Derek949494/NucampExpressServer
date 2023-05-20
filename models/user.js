7b60784559a0ae83c6a6331bc0ad45816ca4f660
Derek <derek.snyder94@gmail.com>
2023-05-20 13:07:16 -0700

    week 4 workshop

--- models/user.js
+++ models/user.js
@@ -5,8 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     firstname: {
         type: String,
-        default: '',
-        facebookId: String,
+        default: ''
     },
     lastname: {
         type: String,
@@ -15,7 +14,8 @@ const userSchema = new Schema({
     admin: {
         type: Boolean,
         default: false
-    }
+    },
+    facebookId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
